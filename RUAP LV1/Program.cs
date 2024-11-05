c8ad99228083cd7ff6c5aaae6fc8ebea442a285b
Filip <filipleskovic159@gmail.com>
2024-11-05 12:15:58 +0100

    fix bug

--- RUAP LV1/Program.cs
+++ RUAP LV1/Program.cs
@@ -1,14 +1,10 @@
-﻿namespace RUAP_LV1
+namespace RUAP_LV1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Filip Lešković. Moj JMBAG je: 12345677899");
-=======
-            Console.WriteLine("Pozdrav iz aplikacije od Filip Lešković");
->>>>>>> 1663ef4 (Add first file)
         }
     }
 }
