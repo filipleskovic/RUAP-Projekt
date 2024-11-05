63e350c322516c0d4f9fb87d056b9f09ce7fde02
Filip <filipleskovic159@gmail.com>
2024-11-05 11:44:29 +0100

    Conflict solved

--- RUAP LV1/Program.cs
+++ RUAP LV1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Filip Lešković. Moj JMBAG je: 12345677899");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Filip Lešković");
+>>>>>>> 1663ef4 (Add first file)
         }
     }
 }
